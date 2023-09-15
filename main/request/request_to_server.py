ae457ebb6ac75c65a7233b3f63eb76cddbcf62b9
iztvmrk <iztvmrk@mail.com>
2023-09-15 14:46:33 +0300

    test

--- main/request/request_to_server.py
+++ main/request/request_to_server.py
@@ -1,5 +1,5 @@
 from aiohttp import ClientSession
-import asyncio
+import requests
 import time
 
 
@@ -7,14 +7,32 @@ from main.core.config import URL, PATH
 from main.request.session import connect, disconnect
 
 
-class Requests_automat:
+class RequestsServer:
 
     def __init__(self):  
         self.url = URL
         self.time_zone = 3
         self.id_request = int(time.time())
-        
-    async def request_automat(self):
+
+    # @property
+    # def request_server(self):
+    #     respond = requests.api.get(
+    #         url = self.url + PATH['state'],
+    #         params = {
+    #             'action': 'Read',
+    #             '_dc': self.id_request
+    #         },
+    #         headers = {
+    #             'Content-Type': 'application/jso',
+    #             'Host': 'erp.ephor.online',
+    #             'Connection': 'keep-alive',
+    #             'Cookie': connect
+    #         }
+    #     )
+    #     return respond.json()
+
+    @property  
+    async def request_server(self):
         async with ClientSession(base_url = self.url) as session:
             async with session.get(
                 url = PATH['state'],
@@ -31,7 +49,24 @@ class Requests_automat:
             ) as respond:
                 disconnect
                 return await respond.json(content_type = 'text/html')
-        
+    
+    # def check_error(self, id):
+    #     respond = requests.api.get(
+    #         url = self.url + PATH['state'],
+    #         params = {
+    #             'action': 'Read',
+    #             '_dc': self.id_request,
+    #             'filter': ('[{"property": "automat_id", "value": %s}]' % id)
+    #         },
+    #         headers = {
+    #             'Content-Type': 'application/jso',
+    #             'Host': 'erp.ephor.online',
+    #             'Connection': 'keep-alive',
+    #             'Cookie': connect
+    #         }
+    #     )
+    #     return respond.json()
+
     async def check_error(self, id):
         async with ClientSession(base_url = self.url) as session:
             async with session.get(
--- main/request/respond_error.py
+++ main/request/respond_error.py
@@ -7,113 +7,113 @@ import logging
 
 from telegram_bot.settings.config import BOT_TOKEN, CHANNEL_ID
 from main.core.config import STATUS
-from main.request.request_to_server import Requests_automat
-from main.request.test import lstlst
+from main.request.request_to_server import RequestsServer
+# from main.request.test import lstlst
 
 
-def search_errors() -> list:
-    request_all = asyncio.run(Requests_automat().request_automat())
-    errors_automat_ids = [
-        automat_param['automat_id'] for automat_param in request_all['data'] if automat_param['automat_state'] == STATUS['error']
-        ]
-    errors_automat = [
-        automat_param for automat_param in request_all['data'] if automat_param['automat_state'] == STATUS['error']
-        ]
-    with open(
-        file = 'main/request/datas.json',
-        mode = 'w+'
-    ) as file:
-        json.dump(errors_automat_ids, file)
-    return errors_automat
+class Responders:
 
-def comparison():
-    try:
+    def __init__(self):
+        self.R_server = asyncio.run(RequestsServer().request_server)
+        self.token = BOT_TOKEN
+        self.chat_id = CHANNEL_ID
+        self.post_method = 'sendMessage'
+
+    def get_params_automat_ERROR(self):
+        automats_ERROR = [param for param in self.R_server['data'] if param['automat_state'] == STATUS['error']]
+        ids_automat_ERROR =  [ids['automat_id'] for ids in automats_ERROR]
         with open(
             file = 'main/request/datas.json',
-            mode = 'r'
+            mode = 'w+'
         ) as file:
-            old_errors = json.load(file)
-            comparison_list = [automat for automat in search_errors() if not automat['automat_id'] in old_errors]
+            json.dump(ids_automat_ERROR, file)
+        return automats_ERROR
+
+    def comparison(self):
+        try:
+            with open(
+                file = 'main/request/datas.json',
+                mode = 'r'
+            ) as file:
+                old_ids = json.load(file)
+            comparison_list = [automat for automat in self.get_params_automat_ERROR() if automat['automat_id'] not in old_ids]
+            print(comparison_list)
             if comparison_list == []:
                 return None
             else:
                 return comparison_list
-    except FileNotFoundError:
-        return search_errors()
+        except FileNotFoundError:
+            return self.get_params_automat_ERROR()
 
-def check_automat_params() -> list:
-    try:
-        errors_automat_list = []
-        for params in comparison():
-            automat_param = {
-                'id': params['automat_id'],
-                'model': params['model_name'],
-                'adress': params['point_adress'],
-                'point': params['point_comment'],
-                'name': params['point_name']
+    def check_automat_params(self) -> list:
+        try:
+            errors_automat_list = []
+            for params in self.comparison():
+                automat_param = {
+                    'id': params['automat_id'],
+                    'model': params['model_name'],
+                    'adress': params['point_adress'],
+                    'point': params['point_comment'],
+                    'name': params['point_name']
+                }
+                errors_automat_list.append(automat_param)
+            return errors_automat_list
+
+        except TypeError:
+            errors_automat_list = []
+            for params in self.get_params_automat_ERROR():
+                automat_param = {
+                    'id': params['automat_id'],
+                    'model': params['model_name'],
+                    'adress': params['point_adress'],
+                    'point': params['point_comment'],
+                    'name': params['point_name']
+                }
+                errors_automat_list.append(automat_param)
+            return errors_automat_list
+
+    def check_automat_errors(self) -> list:
+        for params in self.check_automat_params():
+            get_error = asyncio.run(RequestsServer().check_error(params['id']))
+            error_descriptions = []
+            for error in get_error['data']:
+                error_descriptions.append({'error': error['description']}) 
+            return error_descriptions
+
+    def merge_params(self):
+        check_list = []
+        for params_dict, params_errors in zip(self.check_automat_params(), self.check_automat_errors()):
+            check_list.append(params_dict | params_errors)
+        return check_list
+
+    def send_message(
+        self,
+        message
+    ):
+        response = requests.post(
+            url = 'https://api.telegram.org/bot{0}/{1}'.format(self.token, self.post_method),
+            data = {
+                'chat_id': self.chat_id,
+                'text': message
             }
-            errors_automat_list.append(automat_param)
-        return errors_automat_list
+        )
+        return response.json()
 
-    except TypeError:
-        errors_automat_list = []
-        for params in search_errors():
-            automat_param = {
-                'id': params['automat_id'],
-                'model': params['model_name'],
-                'adress': params['point_adress'],
-                'point': params['point_comment'],
-                'name': params['point_name']
-            }
-            errors_automat_list.append(automat_param)
-        return errors_automat_list
-
-
-def check_automat_errors() -> list:
-    for params in check_automat_params():
-        get_error = asyncio.run(Requests_automat().check_error(params['id']))
-        error_descriptions = []
-        for error in get_error['data']:
-            error_descriptions.append({'error': error['description']})
-        return error_descriptions
-
-def merge_params():
-    check_list = []
-    for params_dict, params_errors in zip(check_automat_params(), check_automat_errors()):
-        check_list.append(params_dict | params_errors)
-    return check_list
-
-def send_message(token, chat_id, message, method):
-    response = requests.post(
-        url = 'https://api.telegram.org/bot{0}/{1}'.format(token, method),
-        data = {
-            'chat_id': chat_id,
-            'text': message
-        }
-    )
-    return response.json()
-
-def listen_errors():
-    while True:
-        if comparison() != None:
-            for error_automat in merge_params():
-                send_message(
-                    token = BOT_TOKEN,
-                    chat_id = CHANNEL_ID,
+    def listen_errors(self):
+        while True:
+            if self.comparison() != None:
+                # self.check_automat_params()
+                # self.check_automat_errors()
+                for error_automat in self.merge_params():
                     message = (
                         f'Автомат № {error_automat["id"]}\n'
                         f'{error_automat["adress"]}\n'
                         f'{error_automat["point"]} --> {error_automat["name"]}\n'
                         f'{error_automat["error"]}'
-                    ),
-                    method = 'sendMessage'
-                )
-                logging.warning(
-                    f'--- AUTOMAT ID: {error_automat["id"]} --- '
-                    f'ADRESS: {error_automat["adress"]} --- '
-                    f'ERROR: {error_automat["error"]} ---\n'
-                    '----------------------------------------------'
-                )
-        time.sleep(60)
+                        ),
+                    logging.warning(f'Автомат № {error_automat["id"]} выпал в ошибку')
+                    # self.send_message(message = message)
+            time.sleep(60)
     
-listen_errors()
+Responders().listen_errors()
+
