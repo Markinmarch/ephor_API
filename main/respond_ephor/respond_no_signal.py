b4a277034a0d29b63202422e0ed388fa067ea5eb
Markinmarch <iztvmrk@mail.ru>
2023-10-02 09:06:26 +0300

    update

--- main/respond_ephor/respond_no_signal.py
+++ main/respond_ephor/respond_no_signal.py
@@ -29,7 +29,6 @@ class RespondErrorSIGNAL(RequestsServer):
                 mode = 'r'
             ) as file:
                 old_ids = json.load(file)
-
             comparison_list = [automat for automat in self.get_automat_error_SIGNAL if automat['automat_id'] not in old_ids]
             return comparison_list     
                
@@ -53,6 +52,12 @@ class RespondErrorSIGNAL(RequestsServer):
 
     @property
     def listen_signal_error(self):
+        ids_automat_NO_SIGNAL =  [ids['automat_id'] for ids in self.get_automat_error_SIGNAL]
+        with open(
+            file = 'main/respond_ephor/ids_errors/signal_error_ids.json',
+            mode = 'w+'
+        ) as file:
+            json.dump(ids_automat_NO_SIGNAL, file)
         for error_automat in self.get_params:
             message = (
                 f'Автомат № {error_automat["id"]}\n'
@@ -61,10 +66,4 @@ class RespondErrorSIGNAL(RequestsServer):
                 f'{error_automat["error"]}'
                 ),
             logging.warning(f'Автомат № {error_automat["id"]}: {error_automat["error"]}')
-            # send_message(message)
-        ids_automat_NO_SIGNAL =  [ids['automat_id'] for ids in self.get_automat_error_SIGNAL]
-        with open(
-            file = 'main/respond_ephor/ids_errors/signal_error_ids.json',
-            mode = 'w+'
-        ) as file:
-            json.dump(ids_automat_NO_SIGNAL, file)
+            send_message(message)
