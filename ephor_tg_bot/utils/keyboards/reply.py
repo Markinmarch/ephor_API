41550e9dc9baf84fad9456ece4132c9034652de6
Markinmarch <iztvmrk@mail.ru>
2023-10-04 17:56:03 +0300

    update keybaords

--- ephor_tg_bot/utils/keyboards/reply.py
+++ ephor_tg_bot/utils/keyboards/reply.py
@@ -19,3 +19,34 @@ get_phone_user = ReplyKeyboardMarkup(
     one_time_keyboard = True,
     selective = True    
 )
+
+route_number = ReplyKeyboardMarkup(
+    keyboard = [
+        [
+            KeyboardButton(
+                text = '1'
+            ),
+            KeyboardButton(
+                text = '2'
+            ),
+            KeyboardButton(
+                text = '3'
+            ),
+            KeyboardButton(
+                text = '4'
+            ),
+            KeyboardButton(
+                text = '5'
+            ),
+            KeyboardButton(
+                text = '6'
+            ),
+            KeyboardButton(
+                text = '7'
+            )
+        ],
+    ],
+    resize_keyboard = True,
+    one_time_keyboard = True,
+    selective = True    
+)
