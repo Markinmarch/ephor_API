0040fa255fe22e2884907546bc5d6f8507de7bb5
Markinmarch <iztvmrk@mail.ru>
2023-10-04 17:56:23 +0300

    update main (if server down)

--- main/core/setting.py
+++ main/core/setting.py
@@ -13,8 +13,13 @@ def main():
     
     from main import respond_ephor
     while True:
-        respond_ephor.respond_error.RespondError().listen_errors
-        respond_ephor.respond_coins.RespondCoinsCount().listen_coins_count
-        respond_ephor.signal_appeared.StatusSignalOK().listen_signal_appeared
-        respond_ephor.respond_no_signal.RespondErrorSIGNAL().listen_signal_error
-        time.sleep(60)
+        try:
+            respond_ephor.respond_error.RespondError().listen_errors
+            respond_ephor.respond_coins.RespondCoinsCount().listen_coins_count
+            respond_ephor.signal_appeared.StatusSignalOK().listen_signal_appeared
+            respond_ephor.respond_no_signal.RespondErrorSIGNAL().listen_signal_error
+            time.sleep(60)
+        except:
+            logging.info('--- Server or web has been breack, please whait to connect ---')
+            time.sleep(600)
+            continue
