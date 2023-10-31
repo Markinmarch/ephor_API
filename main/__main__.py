9396095808c3b6fb26c5a2673ac42910646954b6
Markinmarch <iztvmrk@mail.ru>
2023-10-31 17:56:18 +0300

    update

--- main/__main__.py
+++ main/__main__.py
@@ -1,25 +1,30 @@
-# import logging
-# import asyncio
-# from main.core.setting import logger, main_bot
+import logging
+import asyncio
+from main.core.setting import logger, main_api, main_bot
+from multiprocessing import Process
 
 
 # if __name__ == '__main__':
 #     logging.info('--- Telegram BOT has been started ---')
 #     try:
-#         asyncio.run(main_bot())
+#         main_bot()
 #     except Exception:
 #         import traceback
 #         logger.warning(traceback.format_exc())
 
 
-import logging
-from main.core.setting import logger, main
+# from main.core.setting import logger, main
 
 
 if __name__ == '__main__':
     try:
         logging.info('--- App has been started ---')
-        main()
+        process_api = Process(target = main_api)
+        process_api.start()
+        process_bot = Process(target = main_bot)
+        process_bot.start()
+        process_api.join()
+        process_bot.join()
     except Exception:
         import traceback
         logger.warning(traceback.format_exc())
--- main/core/setting.py
+++ main/core/setting.py
@@ -5,10 +5,13 @@
 
 import logging
 import asyncio
+import time
 from aiogram import Bot, Dispatcher
 
 
 from main.core.config import BOT_TOKEN
+from main.api.request_ephor import basic_request
+from main.api.respond_ephor.respond_error import RespondError
 
 
 
@@ -25,51 +28,16 @@ bot = Bot(
 
 dp = Dispatcher()
 
-# async def main_bot() -> None:
-#     from main.tg_bot import app
-#     from main.api.request_ephor import basic_request
-#     from main.api.respond_ephor.respond_error import RespondError
-#     from main.api.request_ephor.session import Session
-
-#     loop = asyncio.get_event_loop()
-#     loop.create_task(dp.start_polling(bot))
-#     loop.create_task(Session().login())
-#     # respond = RespondError(basic_request)
-#     # loop.create_task(respond)
-#     loop.run_forever()
-#     # await dp.start_polling(bot)
 
 
 
+def main_bot() -> None:
+    asyncio.run(dp.start_polling(bot))
 
-import logging
-import time
-import asyncio
-
-
-logging.basicConfig(
-    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level = logging.INFO
-)
-logger = logging.getLogger(__name__)
-
-
-def main():
-    from main.api.request_ephor import basic_request
-    from main.api.respond_ephor.respond_error import RespondError
+def main_api() -> None:
 
     respond = RespondError(basic_request)
-    asyncio.run(respond.send_errors())
-
-    # from main.api import respond_ephor
-    # while True:
-    #     # try:
-    #     asyncio.run(respond_ephor._get_respond_error.send_errors())
-    #         # respond_ephor.respond_coins.RespondCoinsCount().send_coins_count
-    #         # respond_ephor.signal_appeared.StatusSignalOK().send_signal_appeared
-    #         # respond_ephor.respond_no_signal.RespondErrorSignal().send_signal_error
-    #     # await asyncio.sleep(60)
-    #     # except:
-    #         # logging.info('--- Server or web has been breack, please whait to connect ---')
-    #         # time.sleep(180)
-    #         # continue
+    
+    while True:
+        asyncio.run(respond.send_errors())
+        time.sleep(30)
