b8f7e588ec59fbbcd9545939696d16a0241743af
iztvmrk <iztvmrk@mail.ru>
2023-11-02 01:46:35 +0300

    clear

--- main/tg_bot/app/errors/send_error.py
+++ main/tg_bot/app/errors/send_error.py
@@ -1,28 +1,11 @@
-# from aiogram import types, F
-from aiogram.filters import Command
-import asyncio
-
-
-from main.core.setting import bot, dp
+from main.core.setting import bot
 from main.core.config import TG_ROUTERS
 from main.tg_bot.utils.keyboards.inline import take_task
-# from main.api.respond_ephor import check_errors
 
 
-# async def send_msg(
-#     msg: str,
-#     router: int = 2    
-# ) -> None:
-#     await bot.send_message(
-#         chat_id = TG_ROUTERS[router],
-#         text = msg,
-#         reply_markup = take_task
-#     )
-
-# @dp.message(Command('test'))
 async def send_msg(
-        msg: str,
-        router: int = 2
+    msg: str,
+    router: int = 2
 ) -> None:
     await bot.send_message(
         chat_id = TG_ROUTERS[router],
