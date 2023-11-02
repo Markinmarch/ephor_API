91656f4f79452d16a7fd160018b48fe06dc00c02
Markinmarch <iztvmrk@mail.ru>
2023-11-02 16:34:21 +0300

    refactory

--- core/setting.py
+++ core/setting.py
@@ -32,18 +32,20 @@ async def main_bot() -> None:
     await dp.start_polling(bot)
 
 async def main_api() -> None:
-    logging.info('--- Ephor API has been started ---')
-    from main_api.request_ephor import basic_request
-    from main_api.respond_ephor.respond_error import RespondError
-    from main_api.respond_ephor.respond_coins import RespondCoinsCount
-    
-    respond_error = RespondError(basic_request)
-    respond_coins = RespondCoinsCount(basic_request)
+    from main_api.respond_ephor import (
+        appeared_signal,
+        responder_error,
+        responder_coins,
+        responder_error_signal
+    )
 
+    logging.info('--- Ephor API has been started ---')
     while True:
         try:
-            await respond_error.send_errors()
-            await respond_coins.send_coins_count()
+            await responder_error.send_errors()
+            await responder_coins.send_coins_count()
+            await appeared_signal.send_signal_appeared()
+            await responder_error_signal.send_signal_error()
             await asyncio.sleep(60)
         except:
             time.sleep(120)
--- main_api/respond_ephor/__init__.py
+++ main_api/respond_ephor/__init__.py
@@ -1,16 +1,16 @@
-import asyncio
-
-
 from . import respond_error
 from . import respond_coins
-# from . import send_error
-# from . import respond_no_signal
-# from . import signal_appeared
+from . import respond_no_signal
+from . import signal_appeared
 
-# from main.core.config import PATH, ACTION, FILTER
-# from main.api.respond_ephor.respond_error import RespondError
-# from main.api.request_ephor import basic_request
+from main_api.request_ephor import basic_request
+from main_api.respond_ephor.respond_error import RespondError
+from main_api.respond_ephor.respond_coins import RespondCoinsCount
+from main_api.respond_ephor.respond_no_signal import RespondErrorSignal
+from main_api.respond_ephor.signal_appeared import StatusSignalOK
+    
 
-
-# respond = RespondError(basic_request)
-# check_errors = asyncio.run(respond.send_errors())
+responder_error = RespondError(basic_request)
+responder_coins = RespondCoinsCount(basic_request)
+responder_error_signal = RespondErrorSignal(basic_request)
+appeared_signal = StatusSignalOK()
