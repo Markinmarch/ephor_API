4f19df598af991d8da06a8bfff9e6167c2279d67
Markinmarch <iztvmrk@mail.ru>
2023-11-10 16:31:15 +0300

    add some JS

--- visual/first.js
+++ visual/first.js
@@ -1,11 +1,20 @@
 // coming soon
-window.onload = init;
+window.onload = addName;
 
-function init() {
-    var button = document.getElementById("addButton")
-    button.onclick = handleButtonClick;
+function addName() {
+    var buttonName = document.getElementById("addParams");
+    buttonName.onclick = handleButtonClick;
 }
 
 function handleButtonClick() {
-    alert("You press button");
+    var textInputName = document.getElementById("userName");
+    var userName = textInputName.value;
+    var textInputAge = document.getElementById("userAge");
+    var userAge = textInputAge.value;
+    if (userName == "" || userAge == "") {
+        alert("Введите параметры, пожалуйста!");
+    }
+    else {
+        alert("Ваше имя: " + userName + " Ваш возраст: " + userAge);
+    }
 }
